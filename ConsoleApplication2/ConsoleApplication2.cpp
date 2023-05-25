b2c79e26c4a1476395514ef9235473993613d326
drume <drume@LAPTOP-BF4EJS5E>
2023-05-25 13:54:27 +0200

    Add project files.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,45 @@
+// ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+// #include <opencv2/dnn_superres.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+//using namespace dnn;
+//using namespace dnn_superres;
+
+
+int main() {
+	// Read image
+	string image_path = "C:/Users/drume/OneDrive/Pictures/OpenCVlogo.png";
+	Mat img = imread(image_path, IMREAD_COLOR);
+	// Region to crop
+	Rect roi;
+	roi.x = 300;
+	roi.y = 0;
+	roi.width = img.size().width - 300;
+	roi.height = 30;
+	img = img(roi);
+
+	imshow("Image", img);
+
+	waitKey(0);
+	return 0;
+
+}
+
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
